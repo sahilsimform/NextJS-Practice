c7bd126bc070b2b3d35c7d93c7cb540b7a18a336
sahilsimform <sahilsinghsimform@gmail.com>
2022-06-29 16:36:01 +0530

    Adding files

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
+import { server } from "../config";
 import Head from "next/head";
+import ArticleList from "../components/ArticleList";
 
 export default function Home({ articles }) {
   return (
@@ -7,17 +9,12 @@ export default function Home({ articles }) {
         <title>Web News</title>
       </Head>
       <h1>Welcome to Next app</h1>
-      {articles.map((article) => (
-        <h3 key={article.id}>{article.title}</h3>
-      ))}
+      <ArticleList articles={articles} />
     </div>
   );
 }
-
 export const getStaticProps = async () => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_limit=6`
-  );
+  const res = await fetch(`${server}/api/articles`);
   const articles = await res.json();
 
   return {
@@ -26,3 +23,16 @@ export const getStaticProps = async () => {
     },
   };
 };
+
+// export const getStaticProps = async () => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts?_limit=6`
+//   );
+//   const articles = await res.json();
+
+//   return {
+//     props: {
+//       articles,
+//     },
+//   };
+// };
